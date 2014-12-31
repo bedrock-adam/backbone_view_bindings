a225c22fa76da38620ebd3f64bf5989dccd6061d
Adam Mikulasev <adam@dealfetch.com.au>
2015-01-01 10:39:57 +1100

    remove syphon and replace with stickit bindings

--- js/views/articleView.js
+++ js/views/articleView.js
@@ -1,16 +1,26 @@
 var ArticleView = Backbone.View.extend({
-  initialize: function() {
-    Backbone.Syphon.deserialize(this, this.model.attributes);
-  },
+  tagName: 'form',
+
+  template: _.template($('#article-form-template').html()),
 
   events: {
     'submit': 'submit'
   },
 
+  bindings: {
+    '#article-title': 'title',
+    '#article-content': 'content'
+  },
+
+  render: function() {
+    this.$el.html(this.template(this.model.toJSON()));
+
+    this.stickit();
+
+    return this;
+  },
+
   submit: function(e) {
     e.preventDefault();
-
-    var formData = Backbone.Syphon.serialize(this);
-    this.model.set(formData);
   }
 });
